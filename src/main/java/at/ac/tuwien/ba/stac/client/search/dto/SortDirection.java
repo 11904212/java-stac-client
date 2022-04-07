361282aeec89ac8c3baf98ff478f12aea93e822b
Martin Hungendorfer <e11904212@student.tuwien.ac.at>
2022-04-07 18:45:56 +0200

    added QueryParameterTest

--- src/main/java/at/ac/tuwien/ba/stac/client/search/dto/SortDirection.java
+++ src/main/java/at/ac/tuwien/ba/stac/client/search/dto/SortDirection.java
@@ -1,6 +1,14 @@
 package at.ac.tuwien.ba.stac.client.search.dto;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum SortDirection {
     ASC,
-    DESC
+    DESC;
+
+    @JsonValue
+    @Override
+    public String toString() {
+        return name().toLowerCase();
+    }
 }
