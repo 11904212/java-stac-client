62f8f5654ce6d11295b649510dc6688da195a871
Martin Hungendorfer <e11904212@student.tuwien.ac.at>
2022-08-05 10:22:03 +0200

    doc: added Javadoc to ItemCollection

--- src/main/java/at/ac/tuwien/ba/stac/client/search/ItemCollection.java
+++ src/main/java/at/ac/tuwien/ba/stac/client/search/ItemCollection.java
@@ -5,12 +5,28 @@ import at.ac.tuwien.ba.stac.client.core.Link;
 
 import java.util.List;
 
+/**
+ * A ItemCollection is a container holding items and links which usually represent the result of a search.
+ * @see <a href="https://github.com/radiantearth/stac-api-spec/tree/main/fragments/itemcollection">itemcollection-spec</a>
+ */
 public interface ItemCollection {
 
+    /**
+     * the type of the item-collection (usually FeatureCollection).
+     * @return the type.
+     */
     String getType();
 
+    /**
+     * a possibly-empty list of items.
+     * @return the list of {@link Item}s.
+     */
     List<Item> getItems();
 
+    /**
+     * a list of links related to this item-collection.
+     * @return the list of {@link Link}s.
+     */
     List<Link> getLinks();
 
 }
