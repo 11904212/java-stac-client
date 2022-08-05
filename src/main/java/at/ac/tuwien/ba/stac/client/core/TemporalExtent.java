21999ec94c4d68411dc13ea6a708b4907c6aba0d
Martin Hungendorfer <e11904212@student.tuwien.ac.at>
2022-08-05 10:51:29 +0200

    doc: added Javadoc to Extent

--- src/main/java/at/ac/tuwien/ba/stac/client/core/TemporalExtent.java
+++ src/main/java/at/ac/tuwien/ba/stac/client/core/TemporalExtent.java
@@ -1,8 +1,16 @@
 package at.ac.tuwien.ba.stac.client.core;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
+/**
+ * A temporal-extent contains information about the temporal extents of a {@link Collection}.
+ * @see <a href="https://github.com/radiantearth/stac-spec/blob/master/collection-spec/collection-spec.md#temporal-extent-object">temporal-extent-spec</a>
+ */
 public interface TemporalExtent {
+
+    /**
+     * a list of time intervals covered by the {@link Collection}
+     * @return the list of intervals.
+     */
     List<List<String>> getInterval();
 }
